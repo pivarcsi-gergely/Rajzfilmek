a2e1d30bef8f607d171b7ad59be201eb25d566f7
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-11-17 11:40:21 +0100

    initial commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+require '../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+
+$app = AppFactory::create();
+
+
+
+?>
---
+++ src/routes.php
@@ -0,0 +1,11 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+return function(Slim\App $app) {
+    $app->get('/rajzfilmek', function(Request $request, Response $response){
+    $response->getBody()->write('Nothing here. Yet...');
+    return $response;
+    });
+}
+?>
