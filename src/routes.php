bb5dac5a00882c955f75cf867d7b30849b7c85d5
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-11-17 12:54:10 +0100

    routes-ban a rajzfilm utjat megadtuk

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,14 @@
 <?php
+
+use Petrik\Rajzfilmek\Rajzfilm;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function(Slim\App $app) {
     $app->get('/rajzfilmek', function(Request $request, Response $response){
-    $response->getBody()->write('Nothing here. Yet...');
-    return $response;
+        $rajzfilmek = Rajzfilm::osszes();
+        $kimenet = json_encode($rajzfilmek);
+        $response->withHeader('Content-type', 'application/json')->getBody()->write('Nothing here. Yet...');
+        return $response;
     });
 };
