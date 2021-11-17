05af540f2526866c9f7806384b2e9007fa80ef40
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-11-17 12:54:45 +0100

    write($kimenet)

--- src/routes.php
+++ src/routes.php
@@ -8,7 +8,7 @@ return function(Slim\App $app) {
     $app->get('/rajzfilmek', function(Request $request, Response $response){
         $rajzfilmek = Rajzfilm::osszes();
         $kimenet = json_encode($rajzfilmek);
-        $response->withHeader('Content-type', 'application/json')->getBody()->write('Nothing here. Yet...');
+        $response->withHeader('Content-type', 'application/json')->getBody()->write($kimenet);
         return $response;
     });
 };
